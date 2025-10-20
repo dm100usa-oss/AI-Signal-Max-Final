4fd9f91a97312c4ba2aedf1f4e81befa664b9c59
dm100usa-oss <dm100usa@gmail.com>
2025-10-20 14:12:37 -0400

    Delete app/reviews/page.tsx

--- app/reviews/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ReviewsPage() {
-  return (
-    <main style={{ padding: "80px 20px", textAlign: "center" }}>
-      <h1>Отзывы</h1>
-      <p>Эта страница работает. Позже сюда вернём форму отзывов.</p>
-    </main>
-  );
-}
