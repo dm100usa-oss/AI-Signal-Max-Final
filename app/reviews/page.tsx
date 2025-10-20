f5839782dbf6df62c85fca879e264d2f09865b00
dm100usa-oss <dm100usa@gmail.com>
2025-10-20 13:05:28 -0400

    Update page.tsx

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,12 +1,8 @@
+export const dynamic = "force-dynamic";
 "use client";
 
-import { Suspense } from "react";
-import ReviewsInner from "./ReviewsInner";
+import ReviewsInner from "@/components/ReviewsInner";
 
 export default function ReviewsPage() {
-  return (
-    <Suspense fallback={<div className="text-center mt-20 text-neutral-500">Загрузка отзывов...</div>}>
-      <ReviewsInner />
-    </Suspense>
-  );
+  return <ReviewsInner />;
 }
