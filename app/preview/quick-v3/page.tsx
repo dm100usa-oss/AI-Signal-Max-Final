34be200430a3ef773f94769dd2226a429afeaff9
dm100usa-oss <dm100usa@gmail.com>
2025-12-03 18:45:35 -0500

    Delete app/preview/quick-v3 directory

--- app/preview/quick-v3/page.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from "next/dynamic";
-
-const QuickPreview_v3 = dynamic(() => import("@/components/QuickPreview_v3"), {
-  ssr: false,
-});
-
-export default function Page() {
-  return <QuickPreview_v3 />;
-}
