8bc5c179de68295092bef33f63cb2b2538b45ecc
dm100usa-oss <dm100usa@gmail.com>
2026-03-25 15:12:47 -0400

    Update storage.ts

--- lib/storage.ts
+++ lib/storage.ts
@@ -2,8 +2,8 @@
 import { Redis } from '@upstash/redis';
 
 const redis = new Redis({
-  url: process.env.KV_REST_API_URL!,
-  token: process.env.KV_REST_API_TOKEN!,
+  url: process.env.KV_new_KV_REST_API_URL!,
+  token: process.env.KV_new_KV_REST_API_TOKEN!,
 });
 
 export async function saveData(key: string, value: any, ttlSeconds = 3600) {
